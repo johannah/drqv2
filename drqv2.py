09ba9f217d3a0b08745b299f777a9feda3b3cb36
johannah <jh1736@gmail.com>
2022-01-25 22:15:43 -0500

    added rel_eef_only with projection and no skip connection.

--- drqv2.py
+++ drqv2.py
@@ -133,6 +133,10 @@ class Critic(nn.Module):
         self.kinematic_type = kinematic_type
         if self.kinematic_type == 'eef':
             self.input_dim = feature_dim + action_shape[0] + 3
+        elif self.kinematic_type == 'rel_eef_only':
+            self.input_dim = feature_dim * 2
+            self.kine_projection = nn.Sequential(nn.Linear(3, feature_dim),
+                                                 nn.LayerNorm(feature_dim))
         elif self.kinematic_type == 'abs_rel_eef_only':
             self.input_dim = feature_dim * 2
             self.kine_projection = nn.Sequential(nn.Linear(6, feature_dim),
@@ -184,7 +188,10 @@ class Critic(nn.Module):
         h = self.trunk(obs)
         if self.kinematic_type == 'eef':
             kine = self.kinematic_view_eef(action, body)
-            h_action = torch.cat([h, action, kine], dim=-1)
+            h_action = torch.cat([h, kine], dim=-1)
+        elif self.kinematic_type == 'rel_eef_only':
+            kine = self.kine_projection(self.kinematic_view_eef(action, body))
+            h_action = torch.cat([h, kine], dim=-1)
         elif self.kinematic_type == 'abs_rel_eef_only':
             kine = self.kine_projection(self.kinematic_view_rel_eef(action, body))
             h_action = torch.cat([h, kine], dim=-1)
