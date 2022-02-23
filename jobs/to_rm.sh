e8e1d48a695a436451f2b87577f4efa90930b9b0
Johanna Hansen <jhansen@gra-login3.graham.sharcnet>
2022-02-23 11:32:54 -0500

    add cc test

---
+++ jobs/to_rm.sh
@@ -0,0 +1,7 @@
+rm launch_baseline_lift_5hz.sh
+rm launch_baseline_lift.sh
+rm launch_baseline_reach_5hz.sh
+rm launch_baseline_reach.sh
+rm launch_init.sh
+rm launch_lift.sh
+rm launch_reach_5hz.sh
