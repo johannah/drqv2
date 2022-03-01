fd8b9c6ce5c2ead3b26cd3640c83bd695101f3a1
Johanna Hansen <jhansen@narval3.narval.calcul.quebec>
2022-03-01 14:49:39 -0500

    launching jobs

--- jobs/launch_lift.sh
+++ jobs/launch_lift.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=63G
 #SBATCH --time=47:59:00
 #SBATCH --output=/scratch/jhansen/DH22/lift/job_%A-%a.out
-#SBATCH --array=0-15
+#SBATCH --array=0-25
 
 unset display
 
@@ -18,6 +18,6 @@ experiment=lift
 seeds=(1000 1001 1002 1003 1004)
 num_seeds=5
 # environment
-exps=(none body kine_DH_abs_posquat)
+exps=(none body kine_DH_abs_posquat kine_DH_abs_posquat_detach kine_DH_abs_posquat_control)
 cd $HOME/johannah/DH22/drqv2/
 python train.py task=robosuite_lift_joint_position_img agent.experiment_type=${exps[(($SLURM_ARRAY_TASK_ID / $num_seeds))]} seed=${seeds[(($SLURM_ARRAY_TASK_ID % $num_seeds))]}
